e98f1255afd1549b0abeb6bbe4f5de3cdcf54ec1
Phtran0151 <phuong.tran@tomosia.com>
2021-06-03 15:39:27 +0700

    first commit

---
+++ src/test/java/smartdev/com/spring_boot_user_api/SpringBootUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package smartdev.com.spring_boot_user_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
